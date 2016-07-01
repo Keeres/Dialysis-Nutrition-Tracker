40f6a220e081ef6ff3f033c3b500ba8c6d35afd7
Steven Chen <stevenchen@stevens-macbook-pro.local>
2016-07-01 12:04:14 -0500

    Initial Commit

---
+++ Dialysis Nutrition TrackerTests/Dialysis_Nutrition_TrackerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Dialysis_Nutrition_TrackerTests.swift
+//  Dialysis Nutrition TrackerTests
+//
+//  Created by Steven Chen on 7/1/16.
+//  Copyright © 2016 Steven Chen. All rights reserved.
+//
+
+import XCTest
+@testable import Dialysis_Nutrition_Tracker
+
+class Dialysis_Nutrition_TrackerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
